290ff002d35f91c0783445a3553c4ae6caec0605
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2025-07-09 14:01:28 +0300

    queue

--- queue.php
+++ queue.php
@@ -1,1 +1,21 @@
 <?php
+
+$names_queue = ["Mahmoud", "Ahmed", "Aly"];
+
+function enqueue($array){
+    global $names_queue;
+    array_push($names_queue, $array);
+    return $names_queue;
+}
+
+function dequeue(){
+    global $names_queue;
+    return array_shift($names_queue);
+}
+
+print_r($names_queue);
+
+enqueue("x");
+dequeue();
+
+print_r($names_queue);
